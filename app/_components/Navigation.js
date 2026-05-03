3f7d0e5a87e9ecae8828df5dfa8f3a338fb29eea
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-05-03 14:15:57 +0300

    feat : high light current nav limk

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,33 +1,37 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+  const nav = [
+    {
+      name: "Cabins",
+      href: "/cabins",
+    },
+    {
+      name: "About ",
+      href: "/about",
+    },
+    {
+      name: "Guest area",
+      href: "/account",
+    },
+  ];
+  const pathName = usePathname();
+  console.log("path name : ", pathName);
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
-        <li>
-          <Link
-            href="/cabins"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Cabins
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/about"
-            className="hover:text-accent-400 transition-colors"
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
-        </li>
+        {nav.map((item) => (
+          <li key={item.name}>
+            <Link
+              href={item.href}
+              className={`${pathName === item.href ? "text-accent-400" : ""}  hover:text-accent-400 transition-colors`}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
